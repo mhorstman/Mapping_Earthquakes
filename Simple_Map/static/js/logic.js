fcafd6679e43bea5bfb67fded27be71a561b0e5e
Mike Horstman <tonic99@Mikes-MBP.lan>
2021-03-03 15:33:16 -0500

    Updating mappint multiple points files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,19 +1,19 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Get data from cities.js
+let cityData = cities;
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black', 
-    fillColor: 'yellow',
-    fillOpacity: 0.5
-}).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    L.marker(city.location).addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
