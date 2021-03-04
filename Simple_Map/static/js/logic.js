e1afdc312008c4017ae541edbb8cd24742ee9b24
Mike Horstman <tonic99@Mikes-MBP.lan>
2021-03-03 19:00:02 -0500

    Adding Mapping GeoJSON Points file

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,40 +1,40 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Get data from cities.js
-let cityData = cities;
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40, -90], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [37.6213, -122.3790],
-    [30.1978, -97.6670],
-    [43.6831, -79.6098],
-    [40.6439, -73.7817]
-  ];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: '5,10'
-  }).addTo(map);
-
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     L.circleMarker(city.location, {
-//         radius: (city.population-200000)/100000,
-//         color: 'orange',
-//         weight: 4
-//     })
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2><hr><h3> Population " + city.population.toLocaleString() + "</h3>").addTo(map);
-// });
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h4>Airport Code: " + layer.feature.properties.faa + "</h4><hr><h5>" + "Airport Name: " + layer.feature.properties.name + "</h5>");
+    }
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
