7d44500a3faab60d01c6c3c753c230d3dfbdaab3
Mike Horstman <tonic99@Mikes-MBP.lan>
2021-03-04 15:24:09 -0500

    Updating mapping_geoJSON_linesting files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,31 +16,37 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    "Street": streets,
+    "Light": light,
     "Dark": dark
 };
 
 // Craete the map object with center, zoom and default layer
 let map = L.map('mapid', {
-    center: [30,30],
+    center: [44.0, 0.0],
     zoom: 2,
-    layers: [streets]
+    layers: [dark]
 });
 
 // Pass our map layers into our layers control
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/mhorstman/Mapping_Earthquakes/Mapping_Airports/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/mhorstman/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+};
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-      pointToLayer: function(feature,latlng) {
-          return L.marker(latlng)
-          .bindPopup("<h4>Airport Code: " + feature.properties.faa + "<hr>Airport Name: " + feature.properties.name + "</h4>")
-      }
+    style : myStyle,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h4>Airline: " + layer.feature.properties.airline + "<hr>Destination: " + layer.feature.properties.dst + "</h4>");
+    }
   }).addTo(map);
 });
