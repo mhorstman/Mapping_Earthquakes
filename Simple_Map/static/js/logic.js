24e1daa1b76e0ab971073abf0cdd30e58e560da3
Mike Horstman <tonic99@Mikes-MBP.lan>
2021-03-04 14:23:23 -0500

    Updating to include multiple layers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,17 +1,34 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// We create the dark view title layer that will be an option
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Street": streets,
+    "Dark": dark
+};
+
+// Craete the map object with center, zoom and default layer
+let map = L.map('mapid', {
+    center: [30,30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/mhorstman/Mapping_Earthquakes/Mapping_Airports/majorAirports.json";
@@ -21,11 +38,9 @@ d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-      pointToLayer: function(feature, latlng) {
-        console.log(feature);
-        return L.marker(latlng)
-          .bindPopup("<h5>Airport Code: " + feature.properties.faa + "<hr>" + "Airport Name: " + feature.properties.name + "</h5>")
+      pointToLayer: function(feature,latlng) {
+          return L.marker(latlng)
+          .bindPopup("<h4>Airport Code: " + feature.properties.faa + "<hr>Airport Name: " + feature.properties.name + "</h4>")
       }
   }).addTo(map);
 });
-
