21f5090dea4647d86541253e263be5b180291fda
Mike Horstman <73728419+mhorstman@users.noreply.github.com>
2021-04-14 16:09:57 -0400

    Updating config

--- Earthquake_Challenge/static/js/config.js
+++ Earthquake_Challenge/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWhvcnN0bWFuIiwiYSI6ImNrbHNlamFkajF2c3gyb21wZzhyenNjcHAifQ.mT_Lo0i06oO9vpU4eri8VQ";
+const API_KEY = "pk.eyJ1IjoibWhvcnN0bWFuIiwiYSI6ImNrbTE3czljajEwYXUycG1ydDc4MTVnc28ifQ.7ahduH_7wwawalS2_83XHg";
